162caf60f047622f3c4c0653c23883e8f0a1fe0f
Prabatha <81287902+Prabatha@users.noreply.github.com>
2021-10-21 10:17:24 +0700

    edit listbox dan menambah scrollbar

--- todolist.py
+++ todolist.py
@@ -1,6 +1,7 @@
-import tkinter as tk 
+from tkinter import *
+from tkinter import messagebox
 
-main_window = tk.Tk()
+main_window = Tk()
 main_window.title("TO-DO-LIST-APP")
 main_window.geometry('500x250+500+200')
 main_window.config(bg='#223431')
@@ -8,13 +9,30 @@ main_window.config(bg='#223431')
 def add_task():
    task = entry_task.get()
 
-listbox_task = tk.Listbox(main_window, height=3, width=50)
+listbox_task = Listbox(
+    frame,
+    width=30,
+    height=10,
+    font=('Times', 18),
+    bd=0,
+    fg='#464646',
+    highlightthickness=0,
+    selectbackground='#a6a6a6',
+    activestyle="none",
+    
+)
 listbox_task.pack()
 
-entry_task = tk.Entry(main_window, width=50)
+sb = Scrollbar(frame)
+sb.pack(side=RIGHT, fill=BOTH)
+
+listbox_task.config(yscrollcommand=sb.set)
+sb.config(command=lb.yview)
+
+entry_task = Entry(main_window, width=50)
 entry_task.pack()
 
-button_add_task = tk.Button(main_window, text="Tambah List Kegiatan", width=48, command=add_task)
+button_add_task = Button(main_window, text="Tambah List Kegiatan", width=48, command=add_task)
 button_add_task.pack()
 
 main_window.mainloop()
