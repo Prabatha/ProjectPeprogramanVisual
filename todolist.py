fab6614c25d6a1ba0efb7a3c3d0dc7748c742ddc
Prabatha <81287902+Prabatha@users.noreply.github.com>
2021-10-21 08:50:28 +0700

    Tambah backgorund + tambah setting letak pop up
    Tambah backgorund + tambah setting letak pop up

--- todolist.py
+++ todolist.py
@@ -2,12 +2,12 @@ import tkinter as tk
 
 main_window = tk.Tk()
 main_window.title("TO-DO-LIST-APP")
-main_window.geometry('400x100')
+main_window.geometry('500x250+500+200')
+main_window.config(bg='#223431')
 
 def add_task():
    task = entry_task.get()
 
-#Buat GUInya
 listbox_task = tk.Listbox(main_window, height=3, width=50)
 listbox_task.pack()
 
